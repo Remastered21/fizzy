50f2a6085a9f6c4b69dc8af5accff478990f2cff
Henry <mykim1891@hotmail.com>
2020-07-07 22:38:58 -0700

    Settting goal for first step of the program.

---
+++ master.py
@@ -0,0 +1,4 @@
+# TODO: Write a code that will wait for user input on keyboard. Create response for each keystroke.
+# NOTE: I need to create a way to exit out of the infinite loop of waiting for keystroke, other than ctrl+c.
+
+
