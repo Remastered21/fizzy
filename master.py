f31ae655829b02d33dfc0f1387514cc0579352db
Henry <mykim1891@hotmail.com>
2020-07-09 23:28:12 -0700

    Fixed issue with c++

--- master.py
+++ master.py
@@ -1,5 +1,5 @@
 # TODO: Write a code that will wait for user input on keyboard. Create response for each keystroke.
-# NOTE: I need to create a way to exit out of the infinite loop of waiting for keystroke, other than ctrl+c.  
+# NOTE: I need to create a way to exit out of the infinite loop of waiting for keystroke, other than ctrl+c. 
 
 import keyboard as kb
 import threading
--- play_audio.py
+++ play_audio.py
@@ -1,7 +1,11 @@
 # TODO: Write a code that will play an audio file when a key is pressed.
 
+
+# for keyboard input detection and threading
 import keyboard as kb
 import threading
 
+# For audio playback
+import playsound
 
-
+filename = 'myfile.wav'
